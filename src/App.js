ee7b6904d61846ced3e06332ed95673a4c2ce371
MalikaMarselovna <148754005+MalikaMarselovna@users.noreply.github.com>
2024-05-17 15:37:02 +0600

    change some pages styles

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Layout from "./Components/Layout/Layout";
-
-import CategoryPages from "./pages/CategoryPages";
 import Home from "./pages/Home";
-import About from "./pages/About"
-import ProductPage from "./pages/ProductPage";
+import About from "./pages/About";
 import Cart from "./pages/Cart";
-import Contacts from "./pages/Contacts"
+import Contacts from "./pages/Contacts";
 import Login from "./pages/Login";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from 'react-router-dom'
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Delivery from "./pages/Delivery/Delivery"
-import NotFound from "./pages/NotFound/NotFound"
+import Delivery from "./pages/Delivery/Delivery";
+import NotFound from "./pages/NotFound/NotFound";
 import Menu from "./components/Menu/Menu";
-import Announce from "./components/Announce";
-import Newsletter from "./components/Newsletter";
+import Announce from "./Components/Announce";
+import Newsletter from "./Components/Newsletter";
 import Register from "./pages/Register";
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
 import Products from "./components/Products";
 import { createContext, useEffect, useState } from "react";
 import {
@@ -30,10 +22,8 @@ import {
   onOrdersLoad,
   onProductsLoad,
 } from "./firebase";
-import Product from "./Pages/Product/Product";
-import { Cart } from "./Pages/Cart/Cart";
-import ThankYou from "./Pages/ThankYou/ThankYou";
-import Orders from "./Pages/Orders/Orders";
+import ThankYou from "./pages/ThankYou/ThankYou";
+import Orders from "./pages/Orders/Orders";
 
 export const AppContext = createContext({
   categories: [],
@@ -77,22 +67,30 @@ function App() {
       <AppContext.Provider
         value={{ categories, products, cart, setCart, user, orders }}
       >
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="contacts" element={<Contacts />} />
-            <Route path="category" element={<CategoryPages />} />
-            <Route path="delivery" element={<Delivery />} />
-            <Route path="cart" element={<Cart />} />
-            <Route path="*" element={<NotFound />} />
-            <Route path="login" element={<Login />} />
-            <Route path="productPage" element={<ProductPage />} />
-            <Route path="products" element={<Products />} />
-            <Route path="card" element={<ProductPage />} />
-            <Route path="register" element={<Register />} />
-          </Routes>
-        </Layout>
+        <Router>
+          <Layout>
+            <Announce />
+            <Navbar />
+            <Menu />
+            <main>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="about" element={<About />} />
+                <Route path="contacts" element={<Contacts />} />
+                <Route path="category" element={<CategoryPages />} />
+                <Route path="delivery" element={<Delivery />} />
+                <Route path="cart" element={<Cart />} />
+                <Route path="*" element={<NotFound />} />
+                <Route path="login" element={<Login />} />
+                <Route path="productPage" element={<ProductPage />} />
+                <Route path="products" element={<Products />} />
+                <Route path="register" element={<Register />} />
+              </Routes>
+            </main>
+            <Newsletter />
+            <Footer />
+          </Layout>
+        </Router>
       </AppContext.Provider>
     </div>
   );
